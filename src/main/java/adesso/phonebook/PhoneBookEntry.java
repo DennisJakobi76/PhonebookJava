e9c2490b196b30570ee22fbe8680c76c8f9e9745
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-05-08 17:23:38 +0200

    removed getter and setter and used lombok instead

--- src/main/java/adesso/phonebook/PhoneBookEntry.java
+++ src/main/java/adesso/phonebook/PhoneBookEntry.java
@@ -1,60 +1,18 @@
 package adesso.phonebook;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PhoneBookEntry {
 	private Long id;
 	private String firstName;
 	private String lastName;
 	private String phonePrefix;
 	private String phoneNumber;
-
-	public PhoneBookEntry() {
-	}
-
-	public PhoneBookEntry(Long id, String firstName, String lastName, String phonePrefix, String phoneNumber) {
-		this.id = id;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.phonePrefix = phonePrefix;
-		this.phoneNumber = phoneNumber;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getPhonePrefix() {
-		return phonePrefix;
-	}
-
-	public void setPhonePrefix(String phonePrefix) {
-		this.phonePrefix = phonePrefix;
-	}
-
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
 }
