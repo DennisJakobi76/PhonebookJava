bcdf5f3b347c97b94d1dab69b89fee914934fb4a
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-05-09 13:40:40 +0200

    added h2 db to project

--- src/main/java/adesso/phonebook/PhoneBookEntry.java
+++ src/main/java/adesso/phonebook/PhoneBookEntry.java
@@ -1,18 +1,32 @@
 package adesso.phonebook;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
+@Table(name = "phonebook")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class PhoneBookEntry {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+	@Column(name = "first_name")
 	private String firstName;
+	@Column(name = "last_name")
 	private String lastName;
+	@Column(name = "phone_prefix")
 	private String phonePrefix;
+	@Column(name = "phone_number")
 	private String phoneNumber;
 }
